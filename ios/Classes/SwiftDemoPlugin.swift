89172e2f4b9fb6ab79b179aeb64bf1204f221905
hiteshcustomer <hitesh.landge@customerglu.net>
2022-02-22 16:00:36 +0530

    Added iOS method check
    Added iOS method check in SwiftDemoPlugin.Swift

--- ios/Classes/SwiftDemoPlugin.swift
+++ ios/Classes/SwiftDemoPlugin.swift
@@ -11,19 +11,54 @@ public class SwiftDemoPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
 
-  
-    
-       var userData = [String: AnyHashable]()
-        userData["userId"] = "userId"
-       CustomerGlu.getInstance.registerDevice(userdata: userData, loadcampaigns: false) { success, registrationModel in
-            if success {
-               print("Register Successfully \(String(describing: registrationModel))")
-                CustomerGlu.getInstance.openWallet()
+      if(call.method == "getInstance"){
+          
+      }else if(call.method == "setDefaultBannerImage"){
+          
+      }else if(call.method == "configureLoaderColour"){
+          
+      }else if(call.method == "closeWebviewOnDeeplinkEvent"){
+          
+      }else if(call.method == "disableGluSdk"){
+          
+      }else if(call.method == "isFcmApn"){
+          
+      }else if(call.method == "enablePrecaching"){
+          
+      }else if(call.method == "configureSafeArea"){
 
-            } else {
-                print("error")
-            }
-        }
-  }
+      }else if(call.method == "doRegister"){
+          var userData = [String: AnyHashable]()
+          userData["userId"] = "userId"
+          CustomerGlu.getInstance.registerDevice(userdata: userData, loadcampaigns: false) { success, registrationModel in
+              if success {
+                  print("Register Successfully \(String(describing: registrationModel))")
+                  
+                  
+              } else {
+                  print("error")
+              }
+          }
+          
+      }else if(call.method == "updateProfile"){
+          
+      }else if(call.method == "sendEventData"){
+          
+      }else if(call.method == "displayCustomerGluNotification"){
+          
+      }else if(call.method == "getReferralId"){
+          
+      }else if(call.method == "openWallet"){
+          CustomerGlu.getInstance.openWallet()
+      }else if(call.method == "loadAllCampaigns"){
+          CustomerGlu.getInstance.loadAllCampaigns()
+      }else if(call.method == "loadCampaignById"){
+          
+      }else if(call.method == "loadCampaignsByFilter"){
+          
+      }
+
+
+                                                                                   }
 
 }
--- ios/demo_plugin.podspec
+++ ios/demo_plugin.podspec
@@ -15,7 +15,7 @@ A new Flutter project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.dependency 'CustomerGlu', '0.0.4'
+  s.dependency 'CustomerGlu', '1.0.5'
   s.platform = :ios, '8.0'
 
   # Flutter.framework does not contain a i386 slice.
